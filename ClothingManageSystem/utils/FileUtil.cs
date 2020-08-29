7519daa40589908479cdd3891539e82519c09b2e
yzy <948201156@qq.com>
2020-08-29 09:19:19 +0800

    公共方法

---
+++ ClothingManageSystem/utils/FileUtil.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Threading;
+using System.Windows;
+
+namespace ClothingManageSystem.utils
+{
+    class FileUtil
+    {
+        // <summary>
+        // 取得指定路径下所有目录及文件名称（可递归）
+        // </summary>
+        // <param name="strDir">指定路径</param>
+        // <param name="strFilePattern">要与 strDir 中的文件名匹配的搜索字符串
+        // 例：
+        // “*.abc*”返回扩展名为 .abc、.abcd、.abcde、.abcdef 等的文件。
+        // “*.abcd”只返回扩展名为 .abcd 的文件。
+        // “*.abcde”只返回扩展名为 .abcde 的文件。
+        // “*.abcdef”只返回扩展名为 .abcdef 的文件。
+        // </param>
+        // <param name="arrDirs">查询得到的所有目录ArrayList</param>
+        // <param name="arrFiles">查询得到的所有文件名称ArrayList</param>
+        // <param name="bIsRecursive">是否递归查询</param>
+        public static void GetFileList(string strDir, string strFilePattern, ArrayList arrDirs, ArrayList arrFiles, bool bIsRecursive)
+        {
+
+            if (string.IsNullOrEmpty(strDir))
+            {
+                return;
+            }
+
+            try
+            {
+                //取得指定路径下所有符合条件的文件
+                string[] strFiles = Directory.GetFiles(strDir, strFilePattern);
+                //取得指定路径下的所有目录
+                string[] strDirs = Directory.GetDirectories(strDir);
+                foreach (string name in strFiles)
+                {
+                    arrFiles.Add(name);
+                }
+                foreach (string name in strDirs)
+                {
+                    arrDirs.Add(name);
+                }
+                if (bIsRecursive)
+                {
+                    //递归
+                    if (strDirs.Length > 0)
+                    {
+
+                        foreach (string dir in strDirs)
+                        {
+
+                            GetFileList(dir, strFilePattern, arrDirs, arrFiles, bIsRecursive);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // 异常处理
+                MessageBox.Show("获取图片库 异常：" + ex.Message);
+            }
+        }
+
+        // <summary>
+        //  订单数生成目录，再将路径对应文件复制进生成的目录
+        // </summary>
+        public static string copyFileToPrint(int orderNum, string filePath, string targetPath, string remark)
+        {
+
+            string status = "成功";
+            try
+            {
+                // 生成 日期目录
+                string dateStr = DateTime.Now.ToString("yyyyMMdd");
+                // 生成 订单目录
+                targetPath = AddSeparatorChar(targetPath, dateStr);
+                targetPath = AddSeparatorChar(targetPath, orderNum.ToString());
+                // 目标目录不存在，新建一个
+                if (!Directory.Exists(targetPath))
+                {
+                    Directory.CreateDirectory(targetPath);
+                }
+                // 暂停 0.1 秒 避免进程占用问题
+                Thread.Sleep(100);
+
+                FileInfo fileInfo = new FileInfo(filePath);
+                string targetFileName = Path.GetFileNameWithoutExtension(filePath) + "-" + remark + Path.GetExtension(filePath);
+                targetPath = targetPath + System.IO.Path.DirectorySeparatorChar + targetFileName;
+                fileInfo.CopyTo(targetPath, true);
+            }
+            catch (Exception ex)
+            {
+                status = "失败";
+                Console.WriteLine(ex.Message);
+            }
+            return status;
+        }
+
+        private static string AddSeparatorChar(string targetPath, string path)
+        {
+
+            if (targetPath[targetPath.Length - 1] != System.IO.Path.DirectorySeparatorChar)
+            {
+                targetPath += System.IO.Path.DirectorySeparatorChar;
+            }
+            targetPath += path;
+            if (!string.IsNullOrEmpty(path))
+            {
+                AddSeparatorChar(targetPath, "");
+            }
+            return targetPath;
+        }
+
+        public static void Main(string[] args)
+        {
+            //方法体
+            /*string dir = "E:\\222222";
+            string strFilePattern = "*.*";
+            ArrayList arrDirs = new ArrayList();
+            ArrayList arrFiles = new ArrayList();
+            bool bIsRecursive = true;
+            GetFileList(dir, strFilePattern, arrDirs, arrFiles, bIsRecursive);
+            Console.WriteLine(arrDirs.Count + "目录" +arrDirs[0].ToString());*/
+            //  Console.WriteLine(arrFiles);
+
+            string dir = "E:\\222222\\110 拷贝";
+            string[] dirs = dir.Split('\\');
+            foreach (string str in dirs)
+            {
+
+                Console.WriteLine(str);
+            }
+        }
+
+    }
+}
---
+++ ClothingManageSystem/utils/SQLLiteDBUtil.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections;
+using System.Data;
+using System.Data.SQLite;
+using System.IO;
+using System.Windows;
+
+namespace ClothingManageSystem.utils
+{
+    class SQLLiteDBUtil
+    {
+        // 数据库文件夹
+        static string DbPath = Path.Combine(Directory.GetCurrentDirectory(), "Database");
+
+        //与指定的数据库(实际上就是一个文件)建立连接
+        private static SQLiteConnection CreateDatabaseConnection(string dbName = null)
+        {
+            if (!string.IsNullOrEmpty(DbPath) && !Directory.Exists(DbPath))
+                Directory.CreateDirectory(DbPath);
+            dbName = dbName == null ? "database.db" : dbName;
+            var dbFilePath = Path.Combine(DbPath, dbName);
+            if (!File.Exists(dbFilePath))
+            {
+                File.Create(dbFilePath);
+            }
+            return new SQLiteConnection("DataSource = " + dbFilePath);
+        }
+
+
+        // 使用全局静态变量保存连接
+        private static SQLiteConnection connection = CreateDatabaseConnection();
+
+        // 判断连接是否处于打开状态
+        private static void Open(SQLiteConnection connection)
+        {
+            if (connection.State != System.Data.ConnectionState.Open)
+            {
+                connection.Open();
+            }
+        }
+
+        public static void ExecuteNonQuery(string sql)
+        {
+            // 确保连接打开
+            Open(connection);
+
+            using (var tr = connection.BeginTransaction())
+            {
+                try
+                {
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = sql;
+                        command.ExecuteNonQuery();
+                    }
+                    tr.Commit();
+                }
+                catch (System.Data.SQLite.SQLiteException E)
+                {
+                    tr.Rollback();
+                    Console.WriteLine(E.Message);
+                    MessageBox.Show(E.Message);
+                }
+                finally
+                {
+
+                    Close(connection);
+                }
+            }
+        }
+        public static void BatchExecuteNonQuery(ArrayList sqlList)
+        {
+            // 确保连接打开
+            Open(connection);
+
+            using (var tr = connection.BeginTransaction())
+            {
+                try
+                {
+                    using (var command = connection.CreateCommand())
+                    {
+                        for (int i = 0; i < sqlList.Count; i++)
+                        {
+                            string sql = sqlList[i].ToString().Trim();
+                            if (sql.Length > 1)
+                            {
+                                command.CommandText = sql;
+                                command.ExecuteNonQuery();
+                            }
+                        }
+                    }
+                    tr.Commit();
+                }
+                catch (System.Data.SQLite.SQLiteException E)
+                {
+                    tr.Rollback();
+                    Console.WriteLine(E.Message);
+                    MessageBox.Show(E.Message);
+                }
+                finally
+                {
+
+                    Close(connection);
+                }
+            }
+        }
+        public static DataTable ExecuteQuery(string sql)
+        {
+            DataTable dataTable = new DataTable();
+            // 确保连接打开
+            Open(connection);
+
+            using (var tr = connection.BeginTransaction())
+            {
+                using (var command = connection.CreateCommand())
+                {
+
+                    command.CommandText = sql;
+
+                    // 执行查询会返回一个SQLiteDataReader对象
+                    SQLiteDataReader reader = command.ExecuteReader();
+
+                    //reader.Read()方法会从读出一行匹配的数据到reader中。注意：是一行数据。
+                    while (reader.Read())
+                    {
+                        DataRow dataRow = dataTable.NewRow();
+                        int count = reader.FieldCount;
+                        for (int i = 0; i < count; i++)
+                        {
+                            if (dataTable.Columns.Count < count)
+                            {
+                                dataTable.Columns.Add(new DataColumn(reader.GetName(i)));
+                            }
+                            dataRow[reader.GetName(i)] = reader[i];
+                        }
+                        dataTable.Rows.Add(dataRow);
+                    }
+                }
+                tr.Commit();
+            }
+            return dataTable;
+        }
+
+        // 因为SQLite是文件型数据库，可以直接删除文件。但只要数据库连接没有被回收，就无法删除文件。
+        public static void DeleteDatabase(string dbName)
+        {
+            var path = Path.Combine(DbPath, dbName);
+            connection.Close();
+
+            // 置空，手动GC，并等待GC完成后执行文件删除。
+            connection = null;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            File.Delete(path);
+        }
+
+        public static void Close(SQLiteConnection connection)
+        {
+            connection.Close();
+        }
+
+    }
+}
