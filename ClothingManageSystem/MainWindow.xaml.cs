f2cbf96ed17a420b88794c5ffb5a489bebfa5a51
yzy <948201156@qq.com>
2020-08-29 22:57:47 +0800

    修改

--- ClothingManageSystem/MainWindow.xaml.cs
+++ ClothingManageSystem/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using ClothingManageSystem.entity;
+using ClothingManageSystem.page;
+using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,20 +26,51 @@ namespace ClothingManageSystem
         public MainWindow()
         {
             InitializeComponent();
-        }
-        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            DragMove();
+            var menuRegister = new List<SubItem>();
+            menuRegister.Add(new SubItem("客户", new UserControlCustomers()));
+            menuRegister.Add(new SubItem("供应商", new UserControlProviders()));
+            menuRegister.Add(new SubItem("员工"));
+            menuRegister.Add(new SubItem("产品"));
+            var item6 = new ItemMenu("登记", menuRegister, PackIconKind.Register);
+
+            var menuSchedule = new List<SubItem>();
+            menuSchedule.Add(new SubItem("服务"));
+            menuSchedule.Add(new SubItem("会议"));
+            var item1 = new ItemMenu("预约", menuSchedule, PackIconKind.Schedule);
+
+            var menuReports = new List<SubItem>();
+            menuReports.Add(new SubItem("客户"));
+            menuReports.Add(new SubItem("供应商"));
+            menuReports.Add(new SubItem("产品"));
+            menuReports.Add(new SubItem("库存"));
+            menuReports.Add(new SubItem("销售额"));
+            var item2 = new ItemMenu("报告", menuReports, PackIconKind.FileReport);
+
+            var menuExpenses = new List<SubItem>();
+            menuExpenses.Add(new SubItem("固定资产"));
+            menuExpenses.Add(new SubItem("流动资金"));
+            var item3 = new ItemMenu("费用", menuExpenses, PackIconKind.ShoppingBasket);
+
+            var menuFinancial = new List<SubItem>();
+            menuFinancial.Add(new SubItem("现金流"));
+            var item4 = new ItemMenu("财务", menuFinancial, PackIconKind.ScaleBalance);
+
+            Menu.Children.Add(new UserControlMenuItem(item6, this));
+            Menu.Children.Add(new UserControlMenuItem(item1, this));
+            Menu.Children.Add(new UserControlMenuItem(item2, this));
+            Menu.Children.Add(new UserControlMenuItem(item3, this));
+            Menu.Children.Add(new UserControlMenuItem(item4, this));
         }
 
-        private void Close_Click(object sender, RoutedEventArgs e)
+        internal void SwitchScreen(object sender)
         {
-            this.Close();
-        }
+            var screen = ((UserControl)sender);
 
-        private void ShowWeb_Click(object sender, RoutedEventArgs e)
-        {
-            Process.Start((sender as Hyperlink).Tag.ToString());
+            if (screen != null)
+            {
+                StackPanelMain.Children.Clear();
+                StackPanelMain.Children.Add(screen);
+            }
         }
     }
 }
